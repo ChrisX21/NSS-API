264516ea7110bea08586d606089aad4653303d67
ChrisX21 <chrisalexiev@icloud.com>
2024-08-26 15:46:10 +0300

    Initial Commit

---
+++ NSS-API/src/main/java/com/example/nssapi/NssApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.nssapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NssApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NssApiApplication.class, args);
+    }
+
+}
---
+++ NSS-API/src/test/java/com/example/nssapi/NssApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nssapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NssApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
